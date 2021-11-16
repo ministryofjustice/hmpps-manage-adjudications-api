3608b34b708fb243576ff13a81c4a1b5887221ff
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-11-16 17:40:18 +0000

    NN-3723 more swagger docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/ReportedAdjudicationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/ReportedAdjudicationController.kt
@@ -1,17 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers
 
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
+import io.swagger.annotations.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.ReportedAdjudicationDto
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.ReportedAdjudicationService
 
 @ApiModel("Reported adjudication response")
@@ -35,7 +34,8 @@ class ReportedAdjudicationController {
       reportedAdjudication
     )
   }
-
+  @ApiOperation(value = "Get my reported adjudications a caseload")
+  @ApiImplicitParams(ApiImplicitParam(name = "page", dataType = "java.lang.Integer", paramType = "query", value = "Results page you want to retrieve (0..N). Default 0, e.g. the first page", example = "0"), ApiImplicitParam(name = "size", dataType = "java.lang.Integer", paramType = "query", value = "Number of records per page. Default 20"), ApiImplicitParam(name = "sort", dataType = "java.lang.String", paramType = "query", value = "Sort as combined comma separated property and uppercase direction. Multiple sort params allowed to sort by multiple properties. Default to incidentDate,incidentTime ASC"))
   @GetMapping("/my/agency/{agencyId}")
   fun getMyReportedAdjudications(
     @PathVariable(name = "agencyId") agencyId: String,
