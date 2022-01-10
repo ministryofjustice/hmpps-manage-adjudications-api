8c9b74e5e5099f074f3c86b1a162a39310bbfcaa
sp-watson <steve.watson@digital,justice.gov.uk>
2022-01-10 15:34:13 +0000

    NN-3389: Fix Prison API oauth test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/integration/SpringOauthIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/integration/SpringOauthIntegrationTest.kt
@@ -1,27 +1,44 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.integration
 
 import com.github.tomakehurst.wiremock.client.WireMock
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers.DraftAdjudicationResponse
 
 @ActiveProfiles("test")
 class SpringOauthIntegrationTest : IntegrationTestBase() {
+
+  private lateinit var intTestData: IntTestData
+  private lateinit var draftCreationResponseForAdjudication1: DraftAdjudicationResponse
+  private lateinit var draftCreationResponseForAdjudication2: DraftAdjudicationResponse
+
+  @BeforeEach
+  fun beforeEach() {
+    intTestData = IntTestData(webTestClient, jwtAuthHelper, bankHolidayApiMockServer, prisonApiMockServer)
+
+    draftCreationResponseForAdjudication1 = intTestData.startNewAdjudication(IntTestData.ADJUDICATION_1)
+    intTestData.addIncidentStatement(draftCreationResponseForAdjudication1, IntTestData.ADJUDICATION_1)
+
+    draftCreationResponseForAdjudication2 = intTestData.startNewAdjudication(IntTestData.ADJUDICATION_2)
+    intTestData.addIncidentStatement(draftCreationResponseForAdjudication2, IntTestData.ADJUDICATION_2)
+  }
 
   @Test
   fun `should request a new token for each request`() {
     oAuthMockServer.stubGrantToken()
 
-    webTestClient
-      .get()
-      .uri("/reported-adjudications/1524242")
-      .headers(setHeaders(username = "USER1"))
-      .exchange().expectStatus().is2xxSuccessful
+    intTestData.completeDraftAdjudication(
+      draftCreationResponseForAdjudication1,
+      IntTestData.ADJUDICATION_1,
+      setHeaders(username = "USER1")
+    )
 
-    webTestClient
-      .get()
-      .uri("/reported-adjudications/1524242")
-      .headers(setHeaders(username = "USER2"))
-      .exchange().expectStatus().is2xxSuccessful
+    intTestData.completeDraftAdjudication(
+      draftCreationResponseForAdjudication2,
+      IntTestData.ADJUDICATION_2,
+      setHeaders(username = "USER2")
+    )
 
     oAuthMockServer.verify(
       WireMock.postRequestedFor(WireMock.urlEqualTo("/auth/oauth/token"))
