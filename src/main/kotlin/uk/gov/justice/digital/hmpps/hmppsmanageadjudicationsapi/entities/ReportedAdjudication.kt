6729f2ebc4a2197fe3f55f0551416e9f85c8405f
sp-watson <steve.watson@digital,justice.gov.uk>
2022-01-28 11:58:47 +0000

    NN-3920: Add offences to reported adj DB

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/ReportedAdjudication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/ReportedAdjudication.kt
@@ -1,7 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities
 
 import java.time.LocalDateTime
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -18,4 +22,7 @@ data class ReportedAdjudication(
   var incidentRoleCode: String?,
   var incidentRoleAssociatedPrisonersNumber: String?,
   var statement: String,
+  @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true, fetch = FetchType.EAGER)
+  @JoinColumn(name = "reported_adjudication_fk_id")
+  var offences: MutableList<ReportedOffence>? = null,
 ) : BaseEntity()
