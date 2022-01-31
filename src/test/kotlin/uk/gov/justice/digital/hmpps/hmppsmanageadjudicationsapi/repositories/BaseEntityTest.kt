e686a26bc50dd113596927650670e0f59fd98283
sp-watson <steve.watson@digital,justice.gov.uk>
2022-01-31 16:07:48 +0000

    NN-3905: Fix audit modification data

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/repositories/BaseEntityTest.kt
@@ -0,0 +1,61 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.repositories
+
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.context.annotation.Import
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.AuditConfiguration
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.DraftAdjudication
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.IncidentDetails
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.IncidentRole
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.security.UserDetails
+import java.time.LocalDateTime
+
+@DataJpaTest
+@ActiveProfiles("test")
+@WithMockUser(username = "ITAG_USER")
+@Import(AuditConfiguration::class, UserDetails::class)
+class BaseEntityTest {
+  @Autowired
+  lateinit var entityManager: TestEntityManager
+
+  @Test
+  fun `check the correct audit data is used`() {
+    val draft = newDraft()
+
+    val savedEntity = entityManager.persistAndFlush(draft)
+
+    Thread.sleep(2000)
+
+    savedEntity.incidentRole.roleCode = "25c"
+    val updatedEntity = entityManager.persistAndFlush(savedEntity)
+
+    assertThat(updatedEntity.incidentRole.createdByUserId).isEqualTo("ITAG_USER")
+    assertThat(updatedEntity.incidentRole.modifiedByUserId).isEqualTo("ITAG_USER")
+    assertThat(updatedEntity.incidentRole.modifiedDateTime).isGreaterThan(updatedEntity.incidentRole.createDateTime)
+  }
+
+  private fun newDraft(): DraftAdjudication {
+    return DraftAdjudication(
+      prisonerNumber = "A12345",
+      agencyId = "MDI",
+      incidentDetails = IncidentDetails(
+        locationId = 2,
+        dateTimeOfIncident = DEFAULT_DATE_TIME,
+        handoverDeadline = DEFAULT_DATE_TIME.plusDays(2)
+      ),
+      incidentRole = IncidentRole(
+        roleCode = "25a",
+        associatedPrisonersNumber = "B23456"
+      ),
+    )
+  }
+
+  companion object {
+    val DEFAULT_DATE_TIME = LocalDateTime.of(2021, 10, 3, 10, 10, 22)
+  }
+}
