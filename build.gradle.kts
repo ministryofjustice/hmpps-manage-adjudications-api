d410fa2c8be4e1b72a6373ee3164d879f1074a2c
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-03-15 11:48:04 +0000

    NN-4961 sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.3"
+  id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
+  id("jacoco")
+  id("org.sonarqube") version "4.0.0.2929"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
+}
+
+dependencyCheck {
+  suppressionFiles.add("reactive-suppressions.xml")
+  // Please remove the below suppressions once it has been suppressed in the DependencyCheck plugin (see this issue: https://github.com/jeremylong/DependencyCheck/issues/4616)
+  suppressionFiles.add("postgres-suppressions.xml")
 }
 
 configurations {
@@ -16,11 +25,11 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.apache.commons:commons-text:1.10.0")
   implementation("io.swagger:swagger-annotations:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.14")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.14")
 
-  runtimeOnly("com.h2database:h2:2.1.214")
+  runtimeOnly("com.h2database:h2")
   runtimeOnly("org.flywaydb:flyway-core:9.15.1")
   runtimeOnly("org.postgresql:postgresql:42.5.4")
 
