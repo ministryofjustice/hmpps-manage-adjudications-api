301533ef9fc9b758e97ce0c77edd7aa7d323771f
tim <wright.tim.j@gmail.com>
2023-03-27 14:39:20 +0100

    NN-4999 removing, mainly checking issues with github

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/reported/PunishmentsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/reported/PunishmentsService.kt
@@ -36,10 +36,8 @@ class PunishmentsService(
       when (it.type) {
         PunishmentType.PRIVILEGE -> {
           it.privilegeType ?: throw ValidationException("subtype missing for type PRIVILEGE")
-          when (it.privilegeType) {
-            PrivilegeType.OTHER -> it.otherPrivilege ?: throw ValidationException("description missing for type PRIVILEGE - sub type OTHER")
-            else -> {}
-          }
+          if (it.privilegeType == PrivilegeType.OTHER)
+            it.otherPrivilege ?: throw ValidationException("description missing for type PRIVILEGE - sub type OTHER")
         }
         PunishmentType.EARNINGS -> it.stoppagePercentage ?: throw ValidationException("stoppage percentage missing for type EARNINGS")
         else -> {}
