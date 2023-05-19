7987c09164d11aba09efba1c44be026e9f4d9f7b
Tim Wright <wright.tim.j@gmail.com>
2023-05-19 13:56:43 +0100

    NN-5127 new endpoint for ALO offence edit and submit (#377)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/draft/DraftAdjudicationWorkflowController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/draft/DraftAdjudicationWorkflowController.kt
@@ -2,11 +2,13 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers.dra
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.ErrorResponse
@@ -44,4 +46,36 @@ class DraftAdjudicationWorkflowController(
   @ResponseStatus(HttpStatus.CREATED)
   fun completeDraftAdjudication(@PathVariable(name = "id") id: Long): ReportedAdjudicationDto =
     adjudicationWorkflowService.completeDraftAdjudication(id)
+
+  @PostMapping(value = ["/{id}/alo-offence-details"])
+  @Operation(
+    summary = "Submits updated offence details and completes report.  Used by ALO when they need to amend offence details for submitted reports only",
+    responses = [
+      io.swagger.v3.oas.annotations.responses.ApiResponse(
+        responseCode = "200",
+        description = "Reported adjudication",
+      ),
+      io.swagger.v3.oas.annotations.responses.ApiResponse(
+        responseCode = "415",
+        description = "Not able to process the request because the payload is in a format not supported by this endpoint.",
+        content = [
+          io.swagger.v3.oas.annotations.media.Content(
+            mediaType = "application/json",
+            schema = io.swagger.v3.oas.annotations.media.Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  @PreAuthorize("hasRole('ADJUDICATIONS_REVIEWER') and hasAuthority('SCOPE_write')")
+  @ResponseStatus(HttpStatus.OK)
+  fun aloOffenceUpdate(
+    @PathVariable(name = "id") id: Long,
+    @RequestBody @Valid
+    offenceDetailsRequest: OffenceDetailsRequest,
+  ): ReportedAdjudicationDto =
+    adjudicationWorkflowService.setOffenceDetailsAndCompleteDraft(
+      id = id,
+      offenceDetails = offenceDetailsRequest.offenceDetails,
+    )
 }
