f2c09d4889d7746577a1a84b1aee120538eacc81
tim <wright.tim.j@gmail.com>
2023-06-02 14:57:16 +0100

    NN-5130 wrap event publish with feature flag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishService.kt
@@ -1,32 +1,36 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.FeatureFlagsService
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.ReportedAdjudicationDto
 import java.time.Clock
 import java.time.LocalDateTime
 
 @Service
 class EventPublishService(
+  private val featureFlagsService: FeatureFlagsService,
   private val snsService: SnsService,
   private val auditService: AuditService,
   private val clock: Clock,
 ) {
 
   fun publishEvent(event: AdjudicationDomainEventType, adjudication: ReportedAdjudicationDto) {
-    snsService.publishDomainEvent(
-      event,
-      "${event.description} ${adjudication.adjudicationNumber}",
-      occurredAt = LocalDateTime.now(clock),
-      AdditionalInformation(
-        adjudicationNumber = adjudication.adjudicationNumber.toString(),
-        prisonerNumber = adjudication.prisonerNumber,
-      ),
-    )
+    if (featureFlagsService.isAsyncMode()) {
+      snsService.publishDomainEvent(
+        event,
+        "${event.description} ${adjudication.adjudicationNumber}",
+        occurredAt = LocalDateTime.now(clock),
+        AdditionalInformation(
+          adjudicationNumber = adjudication.adjudicationNumber.toString(),
+          prisonerNumber = adjudication.prisonerNumber,
+        ),
+      )
 
-    auditService.sendMessage(
-      event.auditType,
-      adjudication.adjudicationNumber.toString(),
-      adjudication,
-    )
+      auditService.sendMessage(
+        event.auditType,
+        adjudication.adjudicationNumber.toString(),
+        adjudication,
+      )
+    }
   }
 }
