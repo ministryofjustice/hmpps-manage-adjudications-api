a2102367644655e34bfe5728e475143dfe54b3af
Michael Willis <michael.willis@digital.justice.gov.uk>
2023-07-03 13:15:43 +0100

    NN-5241 Map Prison API endpoints on to Adjudication API (#414)
    * NN-5241 Map Prison API endpoints on to Adjudication API
    
    * NN-5241 KLint
    
    * NN-5241 Add service test

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/AdjudicationSummaryController.kt
@@ -0,0 +1,168 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.constraints.NotNull
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationDetail
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationSearchResponse
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationSummary
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.gateways.Finding
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.SummaryAdjudicationService
+import java.time.LocalDate
+
+@RestController
+@Tag(name = "01. Adjudication Summary")
+@RequestMapping("/adjudications")
+@Validated
+class AdjudicationSummaryController(
+  private val summaryAdjudicationService: SummaryAdjudicationService,
+) {
+
+  @Operation(
+    summary = "Return a specific adjudication for a prisoner reference by charge ID",
+    responses = [
+      io.swagger.v3.oas.annotations.responses.ApiResponse(
+        responseCode = "200",
+        description = "Adjudication returned",
+      ),
+    ],
+  )
+  @GetMapping("/{prisonerNumber}/charge/{chargeId}")
+  fun getAdjudication(
+    @PathVariable("prisonerNumber")
+    @Parameter(
+      description = "prisonerNumber",
+      required = true,
+      example = "A1234AA",
+    )
+    prisonerNumber: @NotNull String,
+    @PathVariable("chargeId")
+    @Parameter(
+      description = "chargeId",
+      required = true,
+    )
+    chargeId: Long,
+  ): AdjudicationDetail {
+    return summaryAdjudicationService.getAdjudication(prisonerNumber, chargeId)
+  }
+
+  @Operation(
+    summary = "Offender adjudications summary (awards and sanctions).",
+    description = "Offender adjudications (awards and sanctions).",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Adjudication returned",
+      ),
+    ],
+  )
+  @GetMapping("/by-booking-id/{bookingId}")
+  fun getAdjudicationSummary(
+    @PathVariable("bookingId")
+    @Parameter(
+      description = "The prisoner booking id",
+      required = true,
+    )
+    bookingId: Long,
+    @RequestParam(
+      value = "awardCutoffDate",
+      required = false,
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Parameter(description = "Only awards ending on or after this date (in YYYY-MM-DD format) will be considered.")
+    awardCutoffDate: LocalDate?,
+    @RequestParam(
+      value = "adjudicationCutoffDate",
+      required = false,
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Parameter(description = "Only proved adjudications ending on or after this date (in YYYY-MM-DD format) will be counted.")
+    adjudicationCutoffDate: LocalDate?,
+  ): AdjudicationSummary {
+    return summaryAdjudicationService.getAdjudicationSummary(
+      bookingId,
+      awardCutoffDate,
+      adjudicationCutoffDate,
+    )
+  }
+
+  @Operation(
+    summary = "Return a list of adjudications for a given prisoner",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Adjudications returned",
+      ),
+    ],
+  )
+  @GetMapping("/{prisonerNumber}/adjudications")
+  fun getAdjudicationsByPrisonerNumber(
+    @PathVariable("prisonerNumber")
+    @Parameter(
+      description = "prisonerNumber",
+      required = true,
+      example = "A1234AA",
+    )
+    prisonerNumber: String,
+    @RequestParam(
+      value = "offenceId",
+      required = false,
+    )
+    @Parameter(description = "An offence id to allow optionally filtering by type of offence")
+    offenceId: Long?,
+    @RequestParam(
+      value = "prisonId",
+      required = false,
+    )
+    @Parameter(description = "An agency id to allow optionally filtering by the prison in which the offence occurred")
+    prisonId: String?,
+    @RequestParam(
+      value = "finding",
+      required = false,
+    )
+    @Parameter(
+      description = "Finding code to allow optionally filtering by type of finding",
+      example = "NOT_PROVEN",
+    )
+    finding: Finding?,
+    @RequestParam(
+      value = "fromDate",
+      required = false,
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Parameter(description = "Adjudications must have been reported on or after this date (in YYYY-MM-DD format).")
+    fromDate: LocalDate?,
+    @RequestParam(
+      value = "toDate",
+      required = false,
+    )
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Parameter(description = "Adjudications must have been reported on or before this date (in YYYY-MM-DD format).")
+    toDate: LocalDate?,
+    @ParameterObject
+    @PageableDefault(size = 20)
+    pageable: Pageable,
+  ): AdjudicationSearchResponse {
+    return summaryAdjudicationService.getAdjudications(
+      prisonerNumber = prisonerNumber,
+      offenceId = offenceId,
+      prisonId = prisonId,
+      finding = finding,
+      fromDate = fromDate,
+      toDate = toDate,
+      pageable = pageable,
+    )
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/SummaryAdjudicationService.kt
@@ -0,0 +1,79 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.FeatureFlagsService
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationDetail
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationSearchResponse
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationSummary
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.gateways.Finding
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.gateways.LegacyNomisGateway
+import java.time.LocalDate
+
+@Service
+@Transactional
+class SummaryAdjudicationService(
+  private val legacyNomisGateway: LegacyNomisGateway,
+  private val featureFlagsService: FeatureFlagsService,
+) {
+  fun getAdjudication(prisonerNumber: String, chargeId: Long): AdjudicationDetail {
+    return if (featureFlagsService.isNomisSourceOfTruth()) {
+      legacyNomisGateway.getAdjudicationDetailForPrisoner(prisonerNumber, chargeId)
+    } else {
+      // TODO: get data from this database!
+      AdjudicationDetail(adjudicationNumber = chargeId)
+    }
+  }
+
+  fun getAdjudications(
+    prisonerNumber: String,
+    offenceId: Long?,
+    prisonId: String?,
+    finding: Finding?,
+    fromDate: LocalDate?,
+    toDate: LocalDate?,
+    pageable: Pageable,
+  ): AdjudicationSearchResponse {
+    return if (featureFlagsService.isNomisSourceOfTruth()) {
+      val response = legacyNomisGateway.getAdjudicationsForPrisoner(
+        prisonerNumber,
+        offenceId,
+        prisonId,
+        finding,
+        fromDate,
+        toDate,
+        pageable,
+      )
+
+      val pageOffset = response.headers.getHeader("Page-Offset")
+      val pageSize = response.headers.getHeader("Page-Limit")
+      val totalRecords = response.headers.getHeader("Total-Records")
+      response.body?.let {
+        AdjudicationSearchResponse(
+          results = PageImpl(it.results, PageRequest.of(pageOffset.toInt() / pageSize.toInt(), pageSize.toInt()), totalRecords.toLong()),
+          offences = it.offences,
+          agencies = it.agencies,
+        )
+      } ?: AdjudicationSearchResponse(results = Page.empty(), offences = listOf(), agencies = listOf())
+    } else {
+      // TODO: get data from this database!
+      AdjudicationSearchResponse(results = Page.empty(), offences = listOf(), agencies = listOf())
+    }
+  }
+
+  private fun HttpHeaders.getHeader(key: String) = this[key]?.get(0) ?: "0"
+
+  fun getAdjudicationSummary(bookingId: Long, awardCutoffDate: LocalDate?, adjudicationCutoffDate: LocalDate?): AdjudicationSummary {
+    return if (featureFlagsService.isNomisSourceOfTruth()) {
+      legacyNomisGateway.getAdjudicationsForPrisonerForBooking(bookingId, awardCutoffDate, adjudicationCutoffDate)
+    } else {
+      // TODO: get data from this database!
+      AdjudicationSummary(bookingId = bookingId, adjudicationCount = 0, awards = listOf())
+    }
+  }
+}
