63354f94efe072a9ed4f55ac446d61993ad74ff1
tim <wright.tim.j@gmail.com>
2023-07-26 18:13:06 +0100

    NN-5399 lint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/AdditionalAssociate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/AdditionalAssociate.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 
-
 @Deprecated("this table is purely to support nomis migration and is readonly for adjudications UI")
 @Entity
 @Table(name = "additional_associate")
