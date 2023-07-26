25fc9083425a71558c46f554bd99c5ddccec8d09
Tim Wright <wright.tim.j@gmail.com>
2023-07-26 15:04:42 +0100

    NN-5389 basic DTO for core adjudication details (#441)
    * NN-5389 basic DTO for core adjudication details
    
    * NN-5389 remove yoi flag

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/utils/MigrationEntityBuilder.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.utils
+
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.AdjudicationMigrateDto
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.MigrateAssociate
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.MigrateOffence
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.MigratePrisoner
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.MigrateVictim
+import java.time.LocalDateTime
+
+class MigrationEntityBuilder {
+
+  fun createAdjudication(
+    agencyIncidentId: Long = 1,
+    oicIncidentId: Long = 1,
+    offenceSequence: Long = 1,
+    prisoner: MigratePrisoner = createPrisoner(),
+    offence: MigrateOffence = createOffence(),
+    victim: MigrateVictim? = null,
+    associate: MigrateAssociate? = null,
+  ): AdjudicationMigrateDto =
+    AdjudicationMigrateDto(
+      agencyIncidentId = agencyIncidentId,
+      oicIncidentId = oicIncidentId,
+      offenceSequence = offenceSequence,
+      bookingId = 1,
+      agencyId = "MDI",
+      incidentDateTime = LocalDateTime.now(),
+      locationId = 1,
+      statement = "some details",
+      prisoner = prisoner,
+      offence = offence,
+      victim = victim,
+      associate = associate,
+    )
+
+  fun createPrisoner(prisonerNumber: String = "AE12345", currentAgencyId: String? = null, gender: String = "MALE"): MigratePrisoner =
+    MigratePrisoner(
+      prisonerNumber = prisonerNumber,
+      currentAgencyId = currentAgencyId,
+      gender = gender,
+    )
+
+  fun createOffence(offenceCode: String = "51:17"): MigrateOffence = MigrateOffence(offenceCode = offenceCode)
+
+  fun createVictim(victimIdentifier: String = "SWATSON_GEN", isStaff: Boolean = true): MigrateVictim = MigrateVictim(victimIdentifier = victimIdentifier, isStaff = isStaff)
+
+  fun createAssociate(): MigrateAssociate = MigrateAssociate(associatedPrisoner = "ZY12345")
+}
