7fa4e7c53d0350836c9c9722fc49b84eb9a34a51
tim <wright.tim.j@gmail.com>
2023-07-26 13:50:44 +0100

    NN-5389 basic DTO for core adjudication details

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/dtos/AdjudicationMigrateDto.kt
@@ -0,0 +1,64 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+@Schema(description = "Adjudication to migrate into service")
+data class AdjudicationMigrateDto(
+  @Schema(description = "The agency incident id")
+  val agencyIncidentId: Long,
+  @Schema(description = "The oic incident id")
+  val oicIncidentId: Long,
+  @Schema(description = "the offence sequence")
+  val offenceSequence: Long,
+  @Schema(description = "booking id on adjudication")
+  val bookingId: Long,
+  @Schema(description = "agency id on adjudication")
+  val agencyId: String,
+  @Schema(description = "the date and time of the incident")
+  val incidentDateTime: LocalDateTime,
+  @Schema(description = "the internal location id of the incident")
+  val locationId: Long,
+  @Schema(description = "the incident statement")
+  val statement: String,
+  @Schema(description = "adjudication under yoi rules")
+  val yoiRulesUsed: Boolean,
+  @Schema(description = "prisoner details")
+  val prisoner: MigratePrisoner,
+  @Schema(description = "offence details")
+  val offence: MigrateOffence,
+  @Schema(description = "optional victim", required = false)
+  val victim: MigrateVictim? = null,
+  @Schema(description = "incident associate", required = false)
+  val associate: MigrateAssociate? = null,
+)
+
+@Schema(description = "prisoner details")
+data class MigratePrisoner(
+  @Schema(description = "prisoner number on adjudication")
+  val prisonerNumber: String,
+  @Schema(description = "agency the prisoner is currently residing, or null if no longer in prison")
+  val currentAgencyId: String?,
+  @Schema(description = "gender of the prisoner")
+  val gender: String,
+)
+
+@Schema(description = "offence details")
+data class MigrateOffence(
+  @Schema(description = "the nomis offence code")
+  val offenceCode: String,
+)
+
+@Schema(description = "victim")
+data class MigrateVictim(
+  @Schema(description = "victims staff username or prisoner number")
+  val victimIdentifier: String,
+  @Schema(description = "flag to indicate if staff, false means prisoner")
+  val isStaff: Boolean,
+)
+
+@Schema(description = "incident associate")
+data class MigrateAssociate(
+  @Schema(description = "associate prisoner number")
+  val associatedPrisoner: String?,
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/utils/MigrateFixtures.kt
@@ -0,0 +1,102 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.utils
+
+class MigrateFixtures {
+  private val migrationEntityBuilder = MigrationEntityBuilder()
+
+  val ADULT_SINGLE_OFFENCE = migrationEntityBuilder.createAdjudication()
+
+  val OFFENCE_NOT_ON_FILE = migrationEntityBuilder.createAdjudication(
+    offence = migrationEntityBuilder.createOffence(offenceCode = "1234"),
+  )
+
+  val WITH_ASSOCIATE = migrationEntityBuilder.createAdjudication(
+    associate = migrationEntityBuilder.createAssociate(),
+  )
+
+  val WITH_STAFF_VICTIM = migrationEntityBuilder.createAdjudication(
+    victim = migrationEntityBuilder.createVictim(),
+  )
+
+  val WITH_PRISONER_VICTIM = migrationEntityBuilder.createAdjudication(
+    victim = migrationEntityBuilder.createVictim(victimIdentifier = "QW12345", isStaff = false),
+  )
+
+  val YOUTH_SINGLE_OFFENCE = migrationEntityBuilder.createAdjudication(yoiRulesUsed = true)
+
+  val INVALID_GENDER = migrationEntityBuilder.createAdjudication(
+    prisoner = migrationEntityBuilder.createPrisoner(gender = "?"),
+  )
+
+  val ADULT_MULITPLE_OFFENCES = listOf(
+    migrationEntityBuilder.createAdjudication(),
+    migrationEntityBuilder.createAdjudication(offenceSequence = 2),
+    migrationEntityBuilder.createAdjudication(offenceSequence = 3),
+  )
+
+  val ADULT_TRANSFER = migrationEntityBuilder.createAdjudication(
+    prisoner = migrationEntityBuilder.createPrisoner(currentAgencyId = "LEI"),
+  )
+
+  val MULTIPLE_OFFENDERS = listOf(
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 2,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AB12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 3,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AC12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 4,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AD12345",
+      ),
+    ),
+  )
+
+  val MULTIPLE_OFFENDERS_AND_OFFENCES = listOf(
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 2,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AB12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 2,
+      offenceSequence = 2,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AB12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 3,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AC12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 3,
+      offenceSequence = 2,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AC12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 4,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AD12345",
+      ),
+    ),
+    migrationEntityBuilder.createAdjudication(
+      oicIncidentId = 4,
+      offenceSequence = 2,
+      prisoner = migrationEntityBuilder.createPrisoner(
+        prisonerNumber = "AD12345",
+      ),
+    ),
+  )
+}
