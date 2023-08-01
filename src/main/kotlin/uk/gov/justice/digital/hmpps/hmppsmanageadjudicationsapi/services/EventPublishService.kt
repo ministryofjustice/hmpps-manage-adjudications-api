91b88449da0db62bbbeaa63a5e388cb2523365d2
Tim Wright <wright.tim.j@gmail.com>
2023-08-01 16:20:16 +0100

    NN-5425 link events to flag (#456)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services
 
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.FeatureFlagsConfig
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.ReportedAdjudicationDto
 import java.time.Clock
 import java.time.LocalDateTime
@@ -10,9 +11,17 @@ class EventPublishService(
   private val snsService: SnsService,
   private val auditService: AuditService,
   private val clock: Clock,
+  private val featureFlagsConfig: FeatureFlagsConfig,
 ) {
 
   fun publishEvent(event: AdjudicationDomainEventType, adjudication: ReportedAdjudicationDto) {
+    when (event) {
+      AdjudicationDomainEventType.ADJUDICATION_CREATED -> if (featureFlagsConfig.adjudications) publish(event, adjudication)
+      else -> publish(event, adjudication)
+    }
+  }
+
+  private fun publish(event: AdjudicationDomainEventType, adjudication: ReportedAdjudicationDto) {
     snsService.publishDomainEvent(
       event,
       "${event.description} ${adjudication.adjudicationNumber}",
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/EventPublishServiceTest.kt
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test
 import org.mockito.kotlin.atLeastOnce
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.FeatureFlagsConfig
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers.TestControllerBase.Companion.REPORTED_ADJUDICATION_DTO
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.reported.ReportedAdjudicationTestBase
 import java.time.Clock
@@ -16,15 +18,18 @@ class EventPublishServiceTest : ReportedAdjudicationTestBase() {
   private val snsService: SnsService = mock()
   private val auditService: AuditService = mock()
   private val clock: Clock = Clock.fixed(Instant.ofEpochMilli(0), ZoneId.systemDefault())
+  private val featureFlagsConfig: FeatureFlagsConfig = mock()
 
   private val eventPublishService = EventPublishService(
     snsService = snsService,
     auditService = auditService,
     clock = clock,
+    featureFlagsConfig = featureFlagsConfig,
   )
 
   @Test
   fun `create adjudication event sends chargeNumber and prisonId`() {
+    whenever(featureFlagsConfig.adjudications).thenReturn(true)
     eventPublishService.publishEvent(AdjudicationDomainEventType.ADJUDICATION_CREATED, REPORTED_ADJUDICATION_DTO)
 
     verify(snsService, atLeastOnce()).publishDomainEvent(
