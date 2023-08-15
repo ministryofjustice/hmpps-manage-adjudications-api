cacf0ac4e048e3eb0759fc1a179c518a5780d90d
Tim Wright <wright.tim.j@gmail.com>
2023-08-15 16:10:42 +0100

    NN-5479 reset nomis outcome code (#479)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/integration/MigrateResetIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/integration/MigrateResetIntTest.kt
@@ -2,10 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.integration
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.HearingOutcomeCode
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.ReportedAdjudicationStatus
 
-@ActiveProfiles("sync")
 class MigrateResetIntTest : SqsIntegrationTestBase() {
   @BeforeEach
   fun setUp() {
@@ -66,5 +65,40 @@ class MigrateResetIntTest : SqsIntegrationTestBase() {
       .jsonPath("$.reportedAdjudication.evidence.size()").isEqualTo(0)
       .jsonPath("$.reportedAdjudication.witnesses.size()").isEqualTo(0)
       .jsonPath("$.reportedAdjudication.offenceDetails.offenceCode").isEqualTo(4001)
+  }
+
+  @Test
+  fun `reset migration - phase 2 resets nomis outcome code`() {
+    prisonApiMockServer.stubCreateHearing(IntegrationTestData.DEFAULT_ADJUDICATION.chargeNumber)
+    prisonApiMockServer.stubNomisHearingResult(IntegrationTestData.DEFAULT_ADJUDICATION.chargeNumber, 100)
+
+    initDataForHearings().createHearing()
+
+    webTestClient.put()
+      .uri("/scheduled-tasks/check-nomis-created-hearing-outcomes-for-locking")
+      .headers(setHeaders())
+      .exchange()
+      .expectStatus().isOk
+
+    migrateRecord(
+      dto = MigrateExistingIntTest.getExistingRecord(
+        oicIncidentId = IntegrationTestData.DEFAULT_ADJUDICATION.chargeNumber.toLong(),
+        prisonerNumber = IntegrationTestData.DEFAULT_ADJUDICATION.prisonerNumber,
+      ),
+    )
+
+    webTestClient.delete()
+      .uri("/reported-adjudications/migrate/reset")
+      .headers(setHeaders(activeCaseload = null, roles = listOf("ROLE_MIGRATE_ADJUDICATIONS")))
+      .exchange()
+      .expectStatus().isOk
+
+    webTestClient.get()
+      .uri("/reported-adjudications/${IntegrationTestData.DEFAULT_ADJUDICATION.chargeNumber}/v2")
+      .headers(setHeaders())
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("$.reportedAdjudication.hearings[0].outcome.code").isEqualTo(HearingOutcomeCode.NOMIS.name)
   }
 }
