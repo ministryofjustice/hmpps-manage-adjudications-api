02b84f98a9c00c510c5af763cdeef74570ec6a0a
tim <wright.tim.j@gmail.com>
2023-11-08 14:16:36 +0000

    latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.7.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
   id("jacoco")
-  kotlin("plugin.spring") version "1.9.10"
-  kotlin("plugin.jpa") version "1.9.10"
+  kotlin("plugin.spring") version "1.9.20"
+  kotlin("plugin.jpa") version "1.9.20"
 }
 
 configurations {
