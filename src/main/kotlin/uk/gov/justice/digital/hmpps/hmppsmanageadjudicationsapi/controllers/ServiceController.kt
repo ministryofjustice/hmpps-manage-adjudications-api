7247804521cb913ec8b0a67176e3bae15cecc4db
Tim Wright <wright.tim.j@gmail.com>
2023-11-24 13:28:38 +0000

    NN-5725 active prisons (#604)
    * NN-5725 active prisons
    
    * NN-5725 rename

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/ServiceController.kt
@@ -0,0 +1,37 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers
+
+import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config.ActivePrisonConfig
+
+@Schema(description = "active prison")
+data class ActivePrison(
+  @Schema(description = "agency id")
+  val agency: String,
+  @Schema(description = "is active or not")
+  val active: Boolean,
+)
+
+@PreAuthorize("hasRole('VIEW_ADJUDICATIONS')")
+@RestController
+@RequestMapping("/service")
+class ServiceController(
+  private val activePrisonConfig: ActivePrisonConfig,
+) {
+
+  @GetMapping("/active")
+  fun isAvailable(
+    @RequestParam("agency", required = true) agency: String,
+  ): ActivePrison {
+    return ActivePrison(
+      agency = agency,
+      active = activePrisonConfig.isActive(
+        agency = agency,
+      ),
+    )
+  }
+}
