ec3678cafedb22910f80c76efb13faff174f3145
Tim Wright <wright.tim.j@gmail.com>
2023-11-24 14:02:56 +0000

    NN-5725 add active agencies to info (#605)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/ActivePrisonsInfo.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config
+
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+
+@Component
+class ActivePrisonsInfo(
+  private val activePrisonConfig: ActivePrisonConfig,
+) : InfoContributor {
+  override fun contribute(builder: Info.Builder?) {
+    builder?.withDetail(
+      "activeAgencies",
+      if (activePrisonConfig.activePrisons.trim().isEmpty()) emptyList() else activePrisonConfig.activePrisons.split(","),
+    )
+  }
+}
