dfcd7308e240e0a8505511bf12de2616cd4c1560
tim <wright.tim.j@gmail.com>
2023-12-06 09:43:16 +0000

    add validation to entitites

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/IncidentRole.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/entities/IncidentRole.kt
@@ -2,12 +2,15 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import org.hibernate.validator.constraints.Length
 
 @Entity
 @Table(name = "incident_role")
 data class IncidentRole(
   override val id: Long? = null,
   var roleCode: String?,
+  @field:Length(max = 7)
   var associatedPrisonersNumber: String?,
+  @field:Length(max = 100)
   var associatedPrisonersName: String?,
 ) : BaseEntity()
