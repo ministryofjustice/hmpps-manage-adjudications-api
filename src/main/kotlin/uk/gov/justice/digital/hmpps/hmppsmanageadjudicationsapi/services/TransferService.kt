94851eb4b2dd1fc2e175c303e56ef287acbdfe5c
tim <wright.tim.j@gmail.com>
2024-01-31 13:46:50 +0000

    format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/TransferService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/TransferService.kt
@@ -32,7 +32,7 @@ class TransferService(
   }
 
   companion object {
-     val transferableStatuses = listOf(
+    val transferableStatuses = listOf(
       ReportedAdjudicationStatus.AWAITING_REVIEW,
       ReportedAdjudicationStatus.UNSCHEDULED,
       ReportedAdjudicationStatus.SCHEDULED,
