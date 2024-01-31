c5fb92143c78f47b0fd28cb8b411a1a0f316e042
tim <wright.tim.j@gmail.com>
2024-01-31 14:06:46 +0000

    NN-5837 repair corrupted merge part 1

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixService.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services
 
 import jakarta.transaction.Transactional
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.HearingOutcomeCode
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.Outcome
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.OutcomeCode
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.ReportedAdjudicationStatus
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.gateways.Finding
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.repositories.ReportedAdjudicationRepository
 
 @Transactional
@@ -14,15 +18,43 @@ class MigrationFixService(
 ) {
 
   fun repair() {
-     repairReferPoliceAdjournShouldBeChargeProved()
+    repairReferPoliceAdjournShouldBeChargeProved()
   }
 
   private fun repairReferPoliceAdjournShouldBeChargeProved() {
     reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(status = ReportedAdjudicationStatus.ADJOURNED)
-      .filter { it.hearings.any { hearing ->  hearing.hearingOutcome?.code == HearingOutcomeCode.REFER_POLICE } }
+      .filter { it.hearings.any { hearing -> hearing.hearingOutcome?.code == HearingOutcomeCode.REFER_POLICE } }
       .filter { it.getOutcomes().none { outcome -> outcome.code == OutcomeCode.SCHEDULE_HEARING } }
-      .forEach { record -> reportedAdjudicationRepository.save(record)
-    }
+      .forEach { record ->
+        val policeReferIdx = record.hearings.indexOfLast { it.hearingOutcome?.code == HearingOutcomeCode.REFER_POLICE }
+        val nextHearingAfter = record.hearings.getOrNull(policeReferIdx + 1)
 
+        nextHearingAfter?.let {
+          it.hearingOutcome?.let { hearingOutcome ->
+            if (hearingOutcome.details == Finding.PROVED.name && hearingOutcome.code == HearingOutcomeCode.ADJOURN && record.hearings.getOrNull(policeReferIdx + 2) == null) {
+              log.info("Repairing ${record.chargeNumber}")
+              val policeReferOutcome = record.getOutcomes().sortedBy { outcome -> outcome.getCreatedDateTime() }.last { outcome -> outcome.code == OutcomeCode.REFER_POLICE }
+              // first of all.  need to add in the referal outcome
+              record.addOutcome(
+                Outcome(code = OutcomeCode.SCHEDULE_HEARING, actualCreatedDate = policeReferOutcome.getCreatedDateTime()!!.plusMinutes(1)),
+              )
+              // second, amend adjourn to complete
+              hearingOutcome.code = HearingOutcomeCode.COMPLETE
+              // add a charge proved outcome.
+              record.addOutcome(
+                Outcome(code = OutcomeCode.CHARGE_PROVED, actualCreatedDate = policeReferOutcome.getCreatedDateTime()!!.plusMinutes(2)),
+              )
+              // set it to charge proved
+              record.status = ReportedAdjudicationStatus.CHARGE_PROVED
+
+              reportedAdjudicationRepository.save(record)
+            }
+          }
+        }
+      }
+  }
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixServiceTest.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services
 
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentCaptor
 import org.mockito.kotlin.any
-import org.mockito.kotlin.atMost
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -22,60 +20,57 @@ import java.time.LocalDateTime
 
 class MigrationFixServiceTest : ReportedAdjudicationTestBase() {
 
-
   private val migrationFixService = MigrationFixService(reportedAdjudicationRepository)
 
   override fun `throws an entity not found if the reported adjudication for the supplied id does not exists`() {
-     //na
+    // na
   }
 
   @Test
-  fun `ignores refer police if they have scheduled hearing` () {
+  fun `ignores refer police if they have scheduled hearing`() {
     whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
       listOf(
         entityBuilder.reportedAdjudication().also {
           it.hearings.first().hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
           it.clearOutcomes()
           it.addOutcome(
-            Outcome(code = OutcomeCode.REFER_POLICE)
+            Outcome(code = OutcomeCode.REFER_POLICE),
           )
           it.addOutcome(
-            Outcome(code = OutcomeCode.SCHEDULE_HEARING)
+            Outcome(code = OutcomeCode.SCHEDULE_HEARING),
           )
-        }
-      )
+        },
+      ),
     )
 
     migrationFixService.repair()
     verify(reportedAdjudicationRepository, never()).save(any())
-
   }
 
   @Test
-  fun `ignores records if it has no refer police outcome` () {
+  fun `ignores records if it has no refer police outcome`() {
     whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
       listOf(
-        entityBuilder.reportedAdjudication()
-      )
+        entityBuilder.reportedAdjudication(),
+      ),
     )
 
     migrationFixService.repair()
     verify(reportedAdjudicationRepository, never()).save(any())
-
   }
 
   @Test
-  fun `ignores a record if no hearing follows the police refer` () {
+  fun `ignores a record if no hearing follows the police refer`() {
     whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
       listOf(
         entityBuilder.reportedAdjudication().also {
           it.hearings.first().hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
           it.clearOutcomes()
           it.addOutcome(
-            Outcome(code = OutcomeCode.REFER_POLICE)
+            Outcome(code = OutcomeCode.REFER_POLICE),
           )
-        }
-      )
+        },
+      ),
     )
 
     migrationFixService.repair()
@@ -83,7 +78,7 @@ class MigrationFixServiceTest : ReportedAdjudicationTestBase() {
   }
 
   @Test
-  fun `ignores records if hearing outcome detail of later hearing is not PROVED` () {
+  fun `ignores records if hearing outcome detail of later hearing is not PROVED`() {
     whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
       listOf(
         entityBuilder.reportedAdjudication().also {
@@ -91,61 +86,81 @@ class MigrationFixServiceTest : ReportedAdjudicationTestBase() {
           it.clearOutcomes()
           it.hearings.clear()
           it.hearings.add(
-            Hearing(dateTimeOfHearing = LocalDateTime.now().minusDays(1), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
-              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
-            )
+            Hearing(
+              dateTimeOfHearing = LocalDateTime.now().minusDays(1),
+              locationId = 1,
+              oicHearingType = OicHearingType.GOV,
+              agencyId = "",
+              chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = ""),
+            ),
           )
           it.hearings.add(
-            Hearing(dateTimeOfHearing = LocalDateTime.now(), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
-              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.ADJOURN, adjudicator = "", details = "something else")
-            )
+            Hearing(
+              dateTimeOfHearing = LocalDateTime.now(),
+              locationId = 1,
+              oicHearingType = OicHearingType.GOV,
+              agencyId = "",
+              chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.ADJOURN, adjudicator = "", details = "something else"),
+            ),
           )
           it.addOutcome(
-            Outcome(code = OutcomeCode.REFER_POLICE)
+            Outcome(code = OutcomeCode.REFER_POLICE),
           )
         },
-      )
+      ),
     )
 
     migrationFixService.repair()
     verify(reportedAdjudicationRepository, never()).save(any())
-
   }
 
-
   @Test
-  fun `add missing next step for refer police - schedule a hearing, outcome should be charge proved and not adjourned, when details of hearing is PROVED` () {
+  fun `add missing next step for refer police - schedule a hearing, outcome should be charge proved and not adjourned, when details of hearing is PROVED`() {
     val argumentCaptor = ArgumentCaptor.forClass(ReportedAdjudication::class.java)
 
     whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(ReportedAdjudicationStatus.ADJOURNED)).thenReturn(
-       listOf(
-         entityBuilder.reportedAdjudication().also {
-           it.status = ReportedAdjudicationStatus.ADJOURNED
-           it.clearOutcomes()
-           it.hearings.clear()
-           it.hearings.add(
-             Hearing(dateTimeOfHearing = LocalDateTime.now().minusDays(1), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
-             hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
-             )
-           )
-           it.hearings.add(
-             Hearing(dateTimeOfHearing = LocalDateTime.now(), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
-               hearingOutcome = HearingOutcome(code = HearingOutcomeCode.ADJOURN, adjudicator = "", details = "PROVED")
-             )
-           )
-           it.addOutcome(
-             Outcome(code = OutcomeCode.REFER_POLICE)
-           )
-         },
-       )
-     )
-    verify(reportedAdjudicationRepository).save(argumentCaptor.capture())
+      listOf(
+        entityBuilder.reportedAdjudication().also {
+          it.status = ReportedAdjudicationStatus.ADJOURNED
+          it.clearOutcomes()
+          it.hearings.clear()
+          it.hearings.add(
+            Hearing(
+              dateTimeOfHearing = LocalDateTime.now().minusDays(1),
+              locationId = 1,
+              oicHearingType = OicHearingType.GOV,
+              agencyId = "",
+              chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = ""),
+            ),
+          )
+          it.hearings.add(
+            Hearing(
+              dateTimeOfHearing = LocalDateTime.now(),
+              locationId = 1,
+              oicHearingType = OicHearingType.GOV,
+              agencyId = "",
+              chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.ADJOURN, adjudicator = "", details = "PROVED"),
+            ),
+          )
+          it.addOutcome(
+            Outcome(code = OutcomeCode.REFER_POLICE, actualCreatedDate = LocalDateTime.now()),
+          )
+        },
+      ),
+    )
 
     migrationFixService.repair()
+    verify(reportedAdjudicationRepository).save(argumentCaptor.capture())
 
-    Assertions.assertThat(argumentCaptor.value.getOutcomes().size).isEqualTo(2)
+    Assertions.assertThat(argumentCaptor.value.getOutcomes().size).isEqualTo(3)
+    Assertions.assertThat(argumentCaptor.value.getOutcomes().sortedBy { it.getCreatedDateTime() }.first().code).isEqualTo(OutcomeCode.REFER_POLICE)
+    Assertions.assertThat(argumentCaptor.value.getOutcomes().sortedBy { it.getCreatedDateTime() }[1].code).isEqualTo(OutcomeCode.SCHEDULE_HEARING)
+    Assertions.assertThat(argumentCaptor.value.getOutcomes().sortedBy { it.getCreatedDateTime() }.last().code).isEqualTo(OutcomeCode.CHARGE_PROVED)
+
     Assertions.assertThat(argumentCaptor.value.status).isEqualTo(ReportedAdjudicationStatus.CHARGE_PROVED)
   }
-
-
 }
