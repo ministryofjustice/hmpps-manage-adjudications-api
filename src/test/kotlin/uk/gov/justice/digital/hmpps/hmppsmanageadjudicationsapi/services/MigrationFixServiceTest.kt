b2da79950483134801fb1b4d744bb8fc0baca063
tim <wright.tim.j@gmail.com>
2024-01-31 13:45:53 +0000

    wip

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/MigrationFixServiceTest.kt
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentCaptor
+import org.mockito.kotlin.any
 import org.mockito.kotlin.atMost
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.Hearing
@@ -29,15 +31,88 @@ class MigrationFixServiceTest : ReportedAdjudicationTestBase() {
 
   @Test
   fun `ignores refer police if they have scheduled hearing` () {
+    whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
+      listOf(
+        entityBuilder.reportedAdjudication().also {
+          it.hearings.first().hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
+          it.clearOutcomes()
+          it.addOutcome(
+            Outcome(code = OutcomeCode.REFER_POLICE)
+          )
+          it.addOutcome(
+            Outcome(code = OutcomeCode.SCHEDULE_HEARING)
+          )
+        }
+      )
+    )
+
+    migrationFixService.repair()
+    verify(reportedAdjudicationRepository, never()).save(any())
 
   }
 
   @Test
   fun `ignores records if it has no refer police outcome` () {
+    whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
+      listOf(
+        entityBuilder.reportedAdjudication()
+      )
+    )
+
+    migrationFixService.repair()
+    verify(reportedAdjudicationRepository, never()).save(any())
 
   }
 
-  //most important case.  fix first
+  @Test
+  fun `ignores a record if no hearing follows the police refer` () {
+    whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
+      listOf(
+        entityBuilder.reportedAdjudication().also {
+          it.hearings.first().hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
+          it.clearOutcomes()
+          it.addOutcome(
+            Outcome(code = OutcomeCode.REFER_POLICE)
+          )
+        }
+      )
+    )
+
+    migrationFixService.repair()
+    verify(reportedAdjudicationRepository, never()).save(any())
+  }
+
+  @Test
+  fun `ignores records if hearing outcome detail of later hearing is not PROVED` () {
+    whenever(reportedAdjudicationRepository.findByMigratedIsFalseAndStatus(any())).thenReturn(
+      listOf(
+        entityBuilder.reportedAdjudication().also {
+          it.status = ReportedAdjudicationStatus.ADJOURNED
+          it.clearOutcomes()
+          it.hearings.clear()
+          it.hearings.add(
+            Hearing(dateTimeOfHearing = LocalDateTime.now().minusDays(1), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.REFER_POLICE, adjudicator = "")
+            )
+          )
+          it.hearings.add(
+            Hearing(dateTimeOfHearing = LocalDateTime.now(), locationId = 1, oicHearingType = OicHearingType.GOV, agencyId = "", chargeNumber = "",
+              hearingOutcome = HearingOutcome(code = HearingOutcomeCode.ADJOURN, adjudicator = "", details = "something else")
+            )
+          )
+          it.addOutcome(
+            Outcome(code = OutcomeCode.REFER_POLICE)
+          )
+        },
+      )
+    )
+
+    migrationFixService.repair()
+    verify(reportedAdjudicationRepository, never()).save(any())
+
+  }
+
+
   @Test
   fun `add missing next step for refer police - schedule a hearing, outcome should be charge proved and not adjourned, when details of hearing is PROVED` () {
     val argumentCaptor = ArgumentCaptor.forClass(ReportedAdjudication::class.java)
