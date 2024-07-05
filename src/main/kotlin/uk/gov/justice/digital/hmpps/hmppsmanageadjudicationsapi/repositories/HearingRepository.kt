b8ed5f5250b9a65ae8562e110e87a4412835d8cb
tim <wright.tim.j@gmail.com>
2024-07-05 15:31:10 +0100

    SAA-1852 new endpoint to get hearings by prisoner

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/repositories/HearingRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/repositories/HearingRepository.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.repositories
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.entities.Hearing
 import java.time.LocalDateTime
 
@@ -10,4 +12,35 @@ interface HearingRepository : CrudRepository<Hearing, Long> {
     start: LocalDateTime,
     end: LocalDateTime,
   ): List<Hearing>
+
+  @Query(
+    value = """
+      SELECT ra.prisoner_number,
+             h.date_time_of_hearing,
+             h.location_id,
+             h.oic_hearing_type,
+             h.id AS hearing_id
+       FROM 
+      reported_adjudications ra
+      JOIN hearing h ON h.reported_adjudication_fk_id = ra.id
+      WHERE ra.prisoner_number in :prisoners
+        AND h.agency_id = :agencyId
+        AND h.date_time_of_hearing BETWEEN :startDate AND :endDate
+    """,
+    nativeQuery = true,
+  )
+  fun getHearingsByPrisoner(
+    @Param("agencyId") agencyId: String,
+    @Param("startDate") startDate: LocalDateTime,
+    @Param("endDate") endDate: LocalDateTime,
+    @Param("prisoners") prisoners: List<String>,
+  ): List<HearingsByPrisoner>
+}
+
+interface HearingsByPrisoner {
+  fun getPrisonerNumber(): String
+  fun getDateTimeOfHearing(): LocalDateTime
+  fun getOicHearingType(): String
+  fun getLocationId(): Long
+  fun getHearingId(): Long
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/reported/HearingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/services/reported/HearingService.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException
 import jakarta.transaction.Transactional
 import jakarta.validation.ValidationException
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers.reported.HearingsByPrisoner
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.HearingDto
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.HearingSummaryDto
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.dtos.OutcomeHistoryDto
@@ -24,6 +25,7 @@ import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.Offence
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.reported.OutcomeService.Companion.lastOutcomeIsRefer
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 @Transactional
 @Service
@@ -122,6 +124,32 @@ class HearingService(
 
     return toHearingSummaries(hearings, adjudicationsMap)
   }
+
+  fun getHearingsByPrisoner(
+    agencyId: String,
+    startDate: LocalDate,
+    endDate: LocalDate,
+    prisoners: List<String>,
+  ): List<HearingsByPrisoner> = hearingRepository.getHearingsByPrisoner(
+    agencyId = agencyId,
+    startDate = startDate.atStartOfDay(),
+    endDate = endDate.atTime(LocalTime.MAX),
+    prisoners = prisoners,
+  ).groupBy { it.getPrisonerNumber() }
+    .map {
+      HearingsByPrisoner(
+        prisonerNumber = it.key,
+        hearings = it.value.map { hearing ->
+          HearingDto(
+            id = hearing.getHearingId(),
+            locationId = hearing.getLocationId(),
+            dateTimeOfHearing = hearing.getDateTimeOfHearing(),
+            oicHearingType = OicHearingType.valueOf(hearing.getOicHearingType()),
+            agencyId = agencyId,
+          )
+        },
+      )
+    }
 
   private fun toHearingSummaries(hearings: List<Hearing>, adjudications: Map<String, ReportedAdjudication>): List<HearingSummaryDto> =
     hearings.map {
