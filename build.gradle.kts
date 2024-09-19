070a713dfcb2bbe1de71a8fc4d534ec479b3d640
cugorji-justice <chukwudi.ugorji@digital.justice.gov.uk>
2024-09-19 11:49:16 +0100

    fixing CVE-2024-38816 vulnerability (#831)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import uk.gov.justice.digital.hmpps.gradle.PortForwardRedisTask
 import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.5"
   id("jacoco")
   kotlin("plugin.spring") version "2.0.10"
   kotlin("plugin.jpa") version "2.0.10"
