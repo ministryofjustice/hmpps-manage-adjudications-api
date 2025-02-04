14785052f8dd8a2309b0838764accd0b2c1df2aa
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-04 13:43:06 +0000

    WIP - fixing missing beans

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/WebClientConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/WebClientConfig.kt
@@ -44,4 +44,5 @@ class WebClientConfig(
   fun prisonerSearchWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder) = builder
     .authorisedWebClient(authorizedClientManager, "prisoner-search-api", prisonerSearchBaseUri, apiTimeout)
     .also { log.info("WEB CLIENT CONFIG: creating prisoner search api web client") }
+
 }
