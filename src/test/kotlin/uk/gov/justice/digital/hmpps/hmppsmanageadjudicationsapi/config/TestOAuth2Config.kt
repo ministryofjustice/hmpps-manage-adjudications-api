ad171b7facddd2f9329a87ad94a206d79f3ec443
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-04 15:50:37 +0000

    WIP - fixing missing beans

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
@@ -1,30 +1,32 @@
 package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config
 
-// import org.mockito.Mockito.mock
-// import org.springframework.boot.test.context.TestConfiguration
-// import org.springframework.context.annotation.Bean
-// import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-// import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-// import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+import org.mockito.Mockito.mock
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 
-// @TestConfiguration
+@TestConfiguration
 class TestOAuth2Config {
 
-//  @Bean
-//  fun clientRegistrationRepository(): ClientRegistrationRepository {
-//    return mock()
-//  }
-//
-//  @Bean
-//  fun authorizedClientRepository(): OAuth2AuthorizedClientRepository {
-//    return mock()
-//  }
-//
-//  @Bean
-//  fun authorizedClientManager(
-//    clientRegistrationRepository: ClientRegistrationRepository,
-//    authorizedClientRepository: OAuth2AuthorizedClientRepository,
-//  ): OAuth2AuthorizedClientManager {
-//    return mock()
-//  }
+  @Bean
+  fun clientRegistrationRepository(): ClientRegistrationRepository {
+    return mock()
+  }
+
+  @Bean
+  fun authorizedClientRepository(): OAuth2AuthorizedClientRepository {
+    return mock()
+  }
+
+  @Bean
+  @Primary
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    authorizedClientRepository: OAuth2AuthorizedClientRepository,
+  ): OAuth2AuthorizedClientManager {
+    return mock()
+  }
 }
