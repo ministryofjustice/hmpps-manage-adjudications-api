3d7134271aab48d88d1a496d94f2f6a3f5562581
cugorji-justice <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-04 18:29:47 +0000

    fixing service start-up errors (#856)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/OAuth2ClientConfig.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient
+import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+
+@Configuration
+class OAuth2ClientConfig {
+
+  @Bean
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    authorizedClientRepository: OAuth2AuthorizedClientRepository,
+  ): OAuth2AuthorizedClientManager {
+    val defaultClientCredentialsTokenResponseClient = DefaultClientCredentialsTokenResponseClient()
+    val authentication = UserContext.getAuthentication()
+
+    defaultClientCredentialsTokenResponseClient.setRequestEntityConverter { grantRequest: OAuth2ClientCredentialsGrantRequest? ->
+      val converter = CustomOAuth2ClientCredentialsGrantRequestEntityConverter()
+      val username = authentication.name
+      converter.enhanceWithUsername(grantRequest, username)
+    }
+
+    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+      .clientCredentials { clientCredentialsGrantBuilder: OAuth2AuthorizedClientProviderBuilder.ClientCredentialsGrantBuilder ->
+        clientCredentialsGrantBuilder.accessTokenResponseClient(
+          defaultClientCredentialsTokenResponseClient,
+        )
+      }
+      .build()
+    val authorizedClientManager = DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository)
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+    return authorizedClientManager
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
@@ -3,13 +3,32 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.config
 import org.mockito.Mockito.mock
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 
 @TestConfiguration
+@Profile("test")
 class TestOAuth2Config {
+
   @Bean
-  fun authorizedClientManager(): OAuth2AuthorizedClientManager {
-    // Return a mock to satisfy the dependency
+  fun clientRegistrationRepository(): ClientRegistrationRepository {
+    return mock()
+  }
+
+  @Bean
+  fun authorizedClientRepository(): OAuth2AuthorizedClientRepository {
+    return mock()
+  }
+
+  @Bean
+  @Primary
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    authorizedClientRepository: OAuth2AuthorizedClientRepository,
+  ): OAuth2AuthorizedClientManager {
     return mock()
   }
 }
