91404c79536bac75d37824d77fbb9878ba75fa57
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-04 14:55:41 +0000

    WIP - fixing missing beans

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/config/TestOAuth2Config.kt
@@ -4,12 +4,27 @@ import org.mockito.Mockito.mock
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 
 @TestConfiguration
 class TestOAuth2Config {
+
   @Bean
-  fun authorizedClientManager(): OAuth2AuthorizedClientManager {
-    // Return a mock to satisfy the dependency
+  fun clientRegistrationRepository(): ClientRegistrationRepository {
+    return mock()
+  }
+
+  @Bean
+  fun authorizedClientRepository(): OAuth2AuthorizedClientRepository {
+    return mock()
+  }
+
+  @Bean
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+    authorizedClientRepository: OAuth2AuthorizedClientRepository
+  ): OAuth2AuthorizedClientManager {
     return mock()
   }
 }
