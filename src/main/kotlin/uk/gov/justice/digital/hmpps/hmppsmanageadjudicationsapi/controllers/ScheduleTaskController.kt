fa2bdd12a4335398220b1646812b5e2246db6ee1
ldlharper <luke.harper@digital.justice.gov.uk>
2025-03-04 13:31:31 +0000

    ADJST-1054 Cleanup after migration. (#870)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/ScheduleTaskController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageadjudicationsapi/controllers/ScheduleTaskController.kt
@@ -3,10 +3,8 @@ package uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.controllers
 import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.TransferMigrationService
 import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.draft.DraftAdjudicationService
 
 @RestController
@@ -15,22 +13,8 @@ import uk.gov.justice.digital.hmpps.hmppsmanageadjudicationsapi.services.draft.D
 @RequestMapping("/scheduled-tasks")
 class ScheduleTaskController(
   private val draftAdjudicationService: DraftAdjudicationService,
-  private val transferMigrationService: TransferMigrationService,
 ) {
 
   @DeleteMapping(value = ["/delete-orphaned-draft-adjudications"])
   fun deleteOrphanedDraftAdjudications(): Unit = draftAdjudicationService.deleteOrphanedDraftAdjudications()
-
-  @PostMapping("/migration-populate-charges")
-  fun populateChargesToMigrate(): Unit = transferMigrationService.setupChargeIds()
-
-  @PostMapping("/migration-process-charges")
-  fun processCharges() {
-    var records = transferMigrationService.getNextRecords()
-    while (records.isNotEmpty()) {
-      records = transferMigrationService.processRecords(records)
-      transferMigrationService.completeProcessing(records)
-      records = transferMigrationService.getNextRecords()
-    }
-  }
 }
