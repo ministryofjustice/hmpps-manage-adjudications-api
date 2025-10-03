89a6fe4eee8648950f69bad621839d5b211db50d
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-10-03 12:08:13 +0100

    updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import uk.gov.justice.digital.hmpps.gradle.PortForwardRedisTask
 import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
   id("jacoco")
   kotlin("plugin.spring") version "2.2.10"
   kotlin("plugin.jpa") version "2.2.10"
