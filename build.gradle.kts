190c50e9ed168b175d031cf9dfb057f7804bd638
cugorji-justice <chukwudi.ugorji@digital.justice.gov.uk>
2025-12-01 10:22:12 +0000

    upgrade to 10.0.0-beta (#928)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import uk.gov.justice.digital.hmpps.gradle.PortForwardRedisTask
 import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.0-beta"
   id("jacoco")
   kotlin("plugin.spring") version "2.2.21"
   kotlin("plugin.jpa") version "2.2.21"
