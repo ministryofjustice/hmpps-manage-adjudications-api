5888977591b4dbe7b26278dd5dcd42b69725da63
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2026-07-27 12:45:36 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,12 @@ repositories {
   mavenCentral()
 }
 
+extra["jackson-2-bom.version"] = "2.22.1"
+extra["jackson-bom.version"] = "3.2.1"
+extra["netty.version"] = "4.2.16.Final"
+extra["httpcore5.version"] = "5.4.3"
+extra["postgresql.version"] = "42.7.12"
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-security")
